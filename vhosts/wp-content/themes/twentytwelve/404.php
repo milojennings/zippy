dac59413126a27e579ad49dfa433cc861db7ab83
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-10-16 13:04:23 -0700

    ryan commit 8

--- vhosts/wp-content/themes/twentytwelve/404.php
+++ vhosts/wp-content/themes/twentytwelve/404.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 			<article id="post-0" class="post error404 no-results not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it? FOR SURE!', 'twentytwelve' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
 				</header>
 
 				<div class="entry-content">
